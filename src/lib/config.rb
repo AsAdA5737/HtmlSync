65d50e4062ab129424ca9d82d6031655c364b859
nobu_k1983 <yoshinobu.ayabe@gmail.com>
2010-03-22 22:07:18 +0900

    modify config.rb

--- src/lib/config.rb
+++ src/lib/config.rb
@@ -1,10 +1,10 @@
 module HtmlSync
 
   # 同期元ディレクトリ
-  SRC_DIR="./internal";
+  SRC_DIR="./src";
 
   # 同期先ディレクトリ  
-  DST_DIR="./external";
+  DST_DIR="./dst";
     
   # 同期を取るファイルの関連を表す。
   # 同期元ファイル名→同期先ファイル名を表す。
